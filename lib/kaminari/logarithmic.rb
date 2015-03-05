82dedd26dadcf4e4f06ddafb274a8bd67fe52ac5
Anton Lee <antoshalee@gmail.com>
2015-03-05 22:43:24 +0300

    Init

---
+++ lib/kaminari/logarithmic.rb
@@ -0,0 +1,7 @@
+require "kaminari/logarithmic/version"
+
+module Kaminari
+  module Logarithmic
+    # Your code goes here...
+  end
+end
---
+++ lib/kaminari/logarithmic/version.rb
@@ -0,0 +1,5 @@
+module Kaminari
+  module Logarithmic
+    VERSION = "0.0.1"
+  end
+end
