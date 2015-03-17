3e619399e81aa2ab665b4ac7d8279fae07c325e5
Anton Lee <antoshalee@gmail.com>
2015-03-17 10:28:06 +0300

    page.logaritmic? breaks usual pagination

--- lib/kaminari/logarithmic/page_proxy_extension.rb
+++ lib/kaminari/logarithmic/page_proxy_extension.rb
@@ -2,6 +2,7 @@ module Kaminari
   module Logarithmic
     module PageProxyExtension
       def logarithmic?
+        return false unless @options[:logarithmic_pages].present?
         @options[:logarithmic_pages].include? @page
       end
     end
